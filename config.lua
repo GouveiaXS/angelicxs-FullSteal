b05534a9b296db26756e028901fa605bfc95eeb6
AngelicXS <99211701+GouveiaXS@users.noreply.github.com>
2023-07-23 15:37:08 -0600

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,12 @@
+----------------------------------------------------------------------
+-- Thanks for supporting AngelicXS Scripts!							--
+-- Support can be found at: https://discord.gg/tQYmqm4xNb			--
+-- More paid scripts at: https://angelicxs.tebex.io/ 				--
+-- More FREE scripts at: https://github.com/GouveiaXS/ 				--
+----------------------------------------------------------------------
+-- Model info: https://docs.fivem.net/docs/game-references/ped-models/
+-- Blip info: https://docs.fivem.net/docs/game-references/blips/
+
 Config = {}
 
 
